f00c3bd5dc371a8af2a6fb161201b841a819a5a2
joao nascimento <joao1991algoritmo@gmail.com>
2018-08-29 12:55:23 -0300

    Criação da camada de dominio. Criação das entidades Livro e Autor

---
+++ WebApi.Domain/Livro.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WebApi.Domain
+{
+    public class Livro
+    {
+        public int Id { get; set; }
+        public string Titulo{ get; set; }
+        public decimal Preco { get; set; }
+        public DateTime DataLancamento { get; set; }
+        public ICollection<Autor> Autores { get; set; }
+    }
+}
